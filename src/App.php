4effa1dbeaf74c46a997513df9d73a1bca41682b
CowMan <pawelniewiarowski2001@gmail.com>
2021-09-15 18:56:20 +0200

    Remove middleware from /login and /register

--- src/App.php
+++ src/App.php
@@ -22,8 +22,8 @@ class App
         $this->router->get('/board', [PageController::class, 'board'], ['before' => AuthMiddleware::class]);
         $this->router->get('/board/:number', [PageController::class, 'edit'], ['before' => AuthMiddleware::class]);
         $this->router->get('/about', [PageController::class, 'index'], ['before' => AuthMiddleware::class]);
-        $this->router->get('/login', [PageController::class, 'login'], ['before' => AuthMiddleware::class]);
-        $this->router->get('/register', [PageController::class, 'register'], ['before' => AuthMiddleware::class]);
+        $this->router->get('/login', [PageController::class, 'login']);
+        $this->router->get('/register', [PageController::class, 'register']);
 
         $this->router->post('/actions/article/delete', [ArticleController::class, 'delete']);
         $this->router->post('/actions/article/add', [ArticleController::class, 'create']);
